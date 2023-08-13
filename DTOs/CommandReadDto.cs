51a1a084c4ea43ce365031608c2df1d55b663970
vectorNull <jose.rodriguez76@protonmail.com>
2023-08-13 15:29:00 -0700

    Command DTOs

---
+++ DTOs/CommandReadDto.cs
@@ -0,0 +1,13 @@
+namespace cmdAPI.DTOs
+{
+    public class CommandReadDto
+    {
+        public int Id { get; set; }
+
+        public string? HowTo { get; set; }
+        
+        public string? Platform { get; set; }
+
+        public string? CommandLine { get; set; }
+    }
+}
