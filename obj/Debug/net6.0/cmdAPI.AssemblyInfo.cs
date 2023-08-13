e2559ae2dfab392a5291d2d8ddf713d037e5afb9
vectorNull <jose.rodriguez76@protonmail.com>
2023-08-13 15:09:42 -0700

    Initial commit

--- obj/Debug/net6.0/cmdAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/cmdAPI.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("c1367340-ee7f-4155-9932-431e8b243d0f")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("cmdAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
